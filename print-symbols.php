40dadb1fdafb1c8ecca0fc2a60c0393bac07c650
Loui Weber <stuart@erpfm.com>
2020-07-25 13:18:09 +0700

    Update print-symbols.php

--- print-symbols.php
+++ print-symbols.php
@@ -2,9 +2,9 @@
     /*
      *
      *  This script will provide you with the means to see what is available in symbols and exchanges.
-     *  Note: This is updated every hour by executium as of 24th July 2020, so try to keep up to date with what is happening.
+     *  Note: This is updated every hour by executium as of 25th July 2020, so try to keep up to date with what is happening.
      *
-     */
+ */
 
     ##/ Get JSON Data
     $data=json_decode(file_get_contents('https://marketdata.executium.com/api/v2/system/symbols'));
